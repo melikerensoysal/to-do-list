a6509df5db77522dbd2ead8f1b6da5046adddb34
melikerensoysal <melikerensoysal@gmail.com>
2026-03-18 18:43:09 +0300

    feat: setup initial UI, theme, and empty state

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const todoForm = document.getElementById('todo-form');
 const taskInput = document.getElementById('task-input');
+const taskList = document.getElementById('task-list'); 
 
 const init = () => {
     todoForm.addEventListener('submit', handleAddTask);
@@ -8,11 +9,23 @@ const init = () => {
 const handleAddTask = (e) => {
     e.preventDefault(); 
     
-    const newTaskText = taskInput.value;
+    const newTaskText = taskInput.value.trim(); 
     
-    console.log("New task:", newTaskText);
+    if (!newTaskText) {
+        return; 
+    }
+
+    createTaskElement(newTaskText);
     
     taskInput.value = ''; 
+};
+
+
+const createTaskElement = (text) => {
+    const li = document.createElement('li');
+    li.textContent = text;
+    
+    taskList.appendChild(li);
 };
 
 init();
